059e063998a70f010c44a128d7ab47f61b9822ee
Deepak Vellingiri <aspiring.us@gmail.com>
2017-03-20 10:28:59 +0530

    after setting up Babel and Transpiler

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,20 @@ module.exports = {
     },
     module: {
         loaders: [
-            { test: /\.css$/, loader: "style!css" }
+            { 
+            	test: /\.css$/, loader: "style-loader!css-loader" 
+            },
+            {
+				      test: /\.js$/,
+				      exclude: /(node_modules|bower_components)/,
+				      use: {
+				        loader: 'babel-loader',
+				        options: {
+				          presets: ['es2015', 'stage-0', 'react', 'env'],
+				          plugins: ['transform-runtime'],
+				        }
+				      }
+				    }
         ]
     }
 };
