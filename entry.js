f2cc9fd192bbb3416d7d9fc9b0416470dd9be68a
Deepak Vellingiri <aspiring.us@gmail.com>
2017-03-20 10:50:25 +0530

    after setting Hello from React

--- entry.js
+++ entry.js
@@ -1,3 +1,16 @@
+// Importing Dependencies
+import React from 'react';
+import { render } from 'react-dom';
+
+// Importing Styles
 import "./style.css";
 
-document.write("It works.");
+class HelloReact extends React.Component {
+	render() {
+		return(
+			<div id="helloReact">Hello from React</div>
+		)
+	}
+}
+
+render(<HelloReact/>, document.getElementById("root-app"));
